4fc613dc54a24a02db66f4dbe46c444a7db71b7f
Vinayaka K V <me.vinayakakv@gmail.com>
2019-07-23 18:50:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     url='https://openweavers.github.io/banyan/',
     license='GPLv3',
     author='Vinyas N S, Vinayaka K V, Monish S R',
-    author_email='vinyasns@gmail.com',
     description='A simple P2P application protocol',
     entry_points={
         'console_scripts': [
